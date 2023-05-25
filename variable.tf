7636b4a8b26a2fcf24a5040a4e7953d29b2f5d39
mani <manideeprampally@gmail.com>
2023-05-25 13:37:06 +0530

    add azurevm-script

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  default = "ait-int"
+}
+variable "location" {
+  default = "Central India"
+}
+variable "vm_size" {
+  default = "Standard_B1s"
+}
+variable "username" {
+  default = "testadmin"
+}
+variable "password" {
+  default = "Password1234!"
+}
